61b71a9868a77f947189f17b82e00db10045091f
Deepak <deepak.hcu07@gmail.com>
2015-12-29 14:44:23 +0800

    Numbers in Ruby

---
+++ RubuTutorial/numbers.rb
@@ -0,0 +1,15 @@
+# This program will demonstrate the Numbers in Ruby
+
+
+puts 5 # print 5. 5 is Fixnum
+
+puts 233344444444444444444555 # this is Bignum
+
+puts 1+2
+
+puts 3/2 #Integer Division
+
+puts 3.0/2 #Floating Point Division
+
+
+
